31254ee2c74ba72110cf7d9adc4d9b77a2fb4309
Karlos Kliemann <karloskliemann@gmail.com>
2023-05-16 06:53:37 -0300

    DOM manipulation with the get results

---
+++ js/mostraVideos.js
@@ -0,0 +1,31 @@
+import { conectaAPI } from "./conectaAPI.js";
+
+const lista = document.querySelector("[data-lista]");
+
+function montaCard (titulo, descricao, url, imagem) {
+    const video = document.createElement("li");
+    video.className = "videos__item";
+    video.innerHTML = 
+    `
+        <iframe width="100%" height="72%" src="${url}"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+        </iframe>
+        <div class="descricao-video">
+            <img src="${imagem}" alt="logo canal alura">
+            <h3>${titulo}</h3>
+            <p>${descricao}</p>
+        </div>
+    `;
+
+    return video;
+}
+
+async function listaVideos() {
+    const listaApi = await conectaAPI.listaVideos();
+    listaApi.forEach(element => lista.appendChild(
+        montaCard(element.titulo, element.descricao, element.url, element.imagem)));
+}
+
+listaVideos()
