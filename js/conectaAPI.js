8fac8e21367239fccb79cfdd0f948ae0319253ac
Karlos Kliemann <karloskliemann@gmail.com>
2023-05-16 07:50:14 -0300

    begin the form action implementation

--- js/conectaAPI.js
+++ js/conectaAPI.js
@@ -4,6 +4,27 @@ async function listaVideos () {
     return conexaoConvertida;
 }
 
+async function criaVideo (titulo, descricao, url, imagem) {
+    const conexao = await fetch ("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "Application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: descricao,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+
+}
+
 export const conectaAPI = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
